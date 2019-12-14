ddd7fc6e89e46265393f09c4b0380af12825eb38
Joël Ammann <joelammann@velox.local>
2019-12-14 13:43:54 +0100

    Update copyright

--- rim.psm1
+++ rim.psm1
@@ -1,6 +1,6 @@
 ﻿<# PowerShell Remote Installation Module for Microsoft Deployment Toolkit
 
-Copyright 2018 Joël Ammann
+Copyright 2020 Joël Ammann
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. 
